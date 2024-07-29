b6265fabde0cd890e80a9b47fca357502ce21cb3
yayatrhyna <yayat.ruhyana@outlook.com>
2024-07-29 20:01:16 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('btn-chat-wa').addEventListener('click', function() {
+    window.open('https://wa.me/6281190083300', '_blank');
+});
+
+document.getElementById('btn-customer-portal').addEventListener('click', function() {
+    window.open('https://client.amc.net.id/', '_blank');
+});
